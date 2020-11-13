3fa67d3298105ebada432b37a591e5f858e74507
y <626445080@qq.com>
2020-11-13 17:15:18 +0800

    init project

--- src/main/java/org/example/maven/HelloGit.java
+++ src/main/java/org/example/maven/HelloGit.java
@@ -1,7 +1,10 @@
 package org.example.maven;
 //2.0
+//push之前需要进行commit
 public class HelloGit {
     public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git!");
         System.out.println("hello git!");
     }
 }
